d810306399235417d737e714bc6762b4bb409f3c
Ubuntu <ubuntu@ip-172-31-10-214.ec2.internal>
2017-09-10 17:26:08 +0000

    changed video link to be served from site not local

--- geri_app/urls.py
+++ geri_app/urls.py
@@ -9,9 +9,9 @@ from django import forms
 from . import views
 
 urlpatterns = [
+	url(r'^patient_search/', views.patient_search, name = "pt_search"),
 	url(r'^pt_upload/', views.pt_upload, name = "patient_upload"),
 	url(r'^pt_upload2/', VolunteerUploadFormPreview(VolunteerUploadForm), name = "patient_upload2"),
-	url(r'^patient_search/', views.patient_search, name = "pt_search"),
 	url(r'^patient_video/', views.pt_video, name="patient_video"),
 	url(r'^pt_find/', views.pt_find, name='pt_find'),
 	url(r'^pt_upload_success/', views.pt_upload_success, name = "patient_upload_success"),
