5fe1f3ad759aef416902841a839c7e2026a4325b
Sacheth Chandramouli <c.sacheth1@gmail.com>
2017-10-02 13:32:56 -0400

    the front page stuff
    sorry its late fam

--- gericare_site/settings.py
+++ gericare_site/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
